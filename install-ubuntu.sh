a0435672759d8c2dfa269f11ead9033dac6e3551
sumanth <sumanth.724@gmail.com>
2017-07-06 11:08:07 -0700

    corrected install-ubuntu.sh for setting up the uWebSockets

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,11 +1,28 @@
-sudo apt-get install libuv1-dev libssl-dev
-git clone https://github.com/uWebSockets/uWebSockets 
+#!/bin/bash
+
+#check if its ubuntu 14.04
+RELEASE="$(lsb_release -s -r)"
+echo "${RELEASE}"
+
+if [ "$RELEASE" == "14.04" ]; then
+  wget http://mirrors.kernel.org/ubuntu/pool/universe/libu/libuv1/libuv1-dev_1.8.0-1_amd64.deb
+  sudo dpkg --install libuv1-dev_1.8.0-1_amd64.deb
+  rm -r libuv1-dev_1.8.0-1_amd64.deb
+
+  wget http://mirrors.kernel.org/ubuntu/pool/universe/libu/libuv1/libuv1_1.8.0-1_amd64.deb
+  sudo dpkg --install libuv1_1.8.0-1_amd64.deb
+  rm -r libuv1_1.8.0-1_amd64.deb
+else
+  sudo apt-get install libuv1-dev libssl-dev
+fi
+
+git clone https://github.com/uWebSockets/uWebSockets
 cd uWebSockets
 git checkout e94b6e1
 mkdir build
 cd build
 cmake ..
-make 
+make
 sudo make install
 cd ..
 cd ..
